6482a67c0ce745090f3505fe7e798ce018e9de6a
Rolando Josue Quijije Banchon <rquijijeb2@unemi.edu.ec>
2021-09-02 21:41:47 -0500

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,21 @@
+# Leer un número y mostrar su tabla de multiplicar.
+
+class Practica:
+    def __init__(self):
+        pass
+        
+    def TablaMulti (self,num):
+        cont = 1
+        band = True
+        while cont <= 10 and band == True :
+            res=num*cont
+            print(num,"x",cont,"=",res)
+            cont+=1
+# Leer una secuencia de números y mostrar la suma de los pares y el producto de los
+# que son múltiplo de 5.
+            
+    
+   
+prueba=Practica()
+prueba.TablaMulti(10)
+secu=[2,5,6,7,4,8,3,9,10] 
